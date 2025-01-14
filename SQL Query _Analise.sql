cfdcd2ad7cd0ba1fb9932ae854bd5530030c03be
Matheus Brito <94205592+Matheusbrts@users.noreply.github.com>
2025-01-14 09:14:40 -0300

    SQL Query _Analise.sql

--- SQL Query _Analise.sql
+++ SQL Query _Analise.sql
@@ -4,14 +4,14 @@ WITH projeto_status AS (
     SELECT 
         id_projeto,
         nome_projeto,
-        orçamento_projeto,  
+        orÃ§amento_projeto,  
         'completo' AS status
     FROM projeto_completo
     UNION ALL
     SELECT 
         id_projeto,
         nome_projeto,
-        orçamento_projeto,  
+        orÃ§amento_projeto,  
         'futuros' AS status
     FROM projeto_futuros
 )
@@ -30,8 +30,8 @@ SELECT
 FROM funcionarios f
 JOIN setores s
     ON f.id_departamento = s.id_departamento
-JOIN atribuições_projeto ap
-    ON ap.id_funcionário = f.id_funcionario  
+JOIN atribuiÃ§Ãµes_projeto ap
+    ON ap.id_funcionÃ¡rio = f.id_funcionario  
 JOIN projeto_status ps
 	ON ps.id_projeto = ap.id_projeto;
 
